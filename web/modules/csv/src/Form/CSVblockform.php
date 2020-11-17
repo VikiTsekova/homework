821775bf43199c7d2cdbdcdf6fc914cde8b552c7
VikiTsekova <viktoriacekova@abv.bg>
2020-11-17 18:29:28 +0200

    open file, get values from file

--- web/modules/csv/src/Form/CSVblockform.php
+++ web/modules/csv/src/Form/CSVblockform.php
@@ -4,6 +4,7 @@ namespace Drupal\csv\Form;
 
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
+Use Drupal\Core\File\FileSystemInterface;
 use Drupal\file\Entity\File;
 
 /**
@@ -36,63 +37,41 @@ class CSVBlockForm extends FormBase {
 			'#type' => 'submit',
 			'#value' => t('Submit'),
 		);
-
-		// $session = \Drupal::request()->getSession();
-
-		// if (empty($session->get('upload'))) {
-		// 	$session->set('upload', 1);
-		// }
-
 		return $form;
 	}
 
-	function session_get($session_name, $default) {
-		if (isset($_SESSION[$session_name])) {
-			return $_SESSION[$session_name];
-		}
-		return $default;
-	}
-	function session_set($session_name, $session_value) {
-		$_SESSION[$session_name] = $session_value;
-	}
-
-	function csv_form_alter(array &$form, FormStateInterface $form_state, $form_id) {
-		$form['#validate'][] = 'csv_validate';
-	}
-
 	public function validateForm(array &$form, FormStateInterface $form_state) {
-		$validators['file_validate_extensions'] = array('csv');
-		$fileData = explode('.', $_FILES['files']['name']['upload']);
-		if( $fileData[1] != 'csv' ){
-			$form_state->setErrorByName('upload', t('Please upload csv file!'));
-		}
-		$tempfile = file_save_upload('upload', $validators );
-		if ( $tempfile ) {
-			 $tempfile = $form_state->getValue('upload');
-		} else {
-			$form_state->setErrorByName('upload', t('The file can not be uploaded!'));
+		$this->file = file_save_upload('upload', $form['upload']['#upload_validators'], FALSE, 0);
+		if (!$this->file) {
+			$form_state->setErrorByName('upload', $this->t('Provided file is not a CSV file or is corrupted.'));
 		}
 	}
 
 	public function submitForm(array &$form, FormStateInterface $form_state) {
-		//$line = variable_get('user_import_line_max', 1000);
-		//ini_set('auto_detect_line_endings', true);
-
 		$file = $form_state->getValue('upload');
-		$f = \Drupal\file\Entity\File::load($file);
-		
-		$file_real_path = \Drupal::service('file_system')->realpath($f->getFileUri());
-		
-		$handle = fopen($file_real_path, "r");
-		
-		$row = fgetcsv( $handle );
-		$columns = [];
 
-		while ( $row = fgetcsv( $handle ) ) {
-		// $row is an array of elements in each row
-		// e.g. if the first column is the email address of the user, try something like
-		//$mail = $row[0];
+		$valid = array ('file_validate_extensions' => array('csv'), );
+		$location = 'homework/admin/jira/';
+		$des = $location . time() . '-' . $_FILES['files']['name']['upload'];
+		$new_file = file_save_upload( 'upload', $valid, $des );
+		if($new_file) {
+			$handle = fopen($new_file[0]->getFileUri(), "r");
+			$arr = array();
+			if ( $handle !== FALSE ) {
+				$all = array();
+				$header = fgetcsv($handle, ',');
+				while ( $columns = fgetcsv($handle, ',') ) {
+					$all = array_combine($header, $columns);
+					$arr[] = $all;
+				}
+			}
+			var_dump($arr);
+			$header = file_move();
+			fclose($handle);
+		} else {
+			$form_state->setErrorByName('upload', t('Failed to read the uploaded file.'));
 		}
+		return $arr;
 	}
 
 }
